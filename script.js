f9774fd3bc3bd02b5f8b370ad87d068a5f54b592
eduardopontesdev <eduardopontesdev@gmail.com>
2025-01-06 18:29:40 -0300

    envio do projeto

---
+++ script.js
@@ -0,0 +1,96 @@
+const seletor = document.querySelector(".resultado");
+const saidaSemana = document.getElementById("saidaSemana");
+const saidaSabado = document.getElementById("saidaSabado");
+const chegadaSemana = document.getElementById("chegadaSemana");
+const chegadaSabado = document.getElementById("chegadaSabado");
+const rota = document.getElementById("rota");
+const selectHorario = document.getElementById("listaHorarios");
+const optionHorario = document.getElementById("option-horario");
+const topics = [
+  {
+    //chaval a camocim
+    topic: 1,
+    rota: "Chaval - Camocim",
+    segundaasexta: {
+      saida: ["06:00", "06:30", "07:00", "09:30", "11:40", "12:50"],
+      chegada: ["07:00", "07:30", "08:00", "10:30", "12:40", "13:50"],
+    },
+    sabado: {
+      saida: ["07:10"],
+      chegada: ["08:10"],
+    },
+  },
+  //camocim a chaval
+  {
+    topic: 2,
+    rota: "Camocim - Chaval",
+    segundaasexta: {
+      saida: ["07:50", "09:50", "10:50", "11:40", "16:30", "12:30"],
+      chegada: ["08:50", "10:50", "11:50", "12:40", "15:30", "17:30"],
+    },
+    sabado: {
+      saida: ["11:30"],
+      chegada: ["12:30"],
+    },
+  },
+];
+
+function optionHorarios() {
+  topics.forEach(function (topic) {
+    const option = document.createElement("option");
+    option.value = topic.topic;
+    option.textContent = topic.rota;
+
+    selectHorario.appendChild(option);
+  });
+}
+
+function pegaHorario(item) {
+  if (item != 0) {
+    saidaSemana.innerHTML = "";
+    chegadaSemana.innerHTML = "";
+    saidaSabado.innerHTML = "";
+    chegadaSabado.innerHTML = "";
+    seletor.classList.remove("hide");
+    topics.forEach((topic) => {
+      if (topic.topic === item) {
+        rota.innerHTML = topic.rota;
+        for (let i = 0; i < topic.segundaasexta.saida.length; i++) {
+          const span = document.createElement("li");
+          span.className = "saida-dia";
+          span.innerHTML = topic.segundaasexta.saida[i];
+          saidaSemana.appendChild(span);
+        }
+        for (let i = 0; i < topic.segundaasexta.chegada.length; i++) {
+          const span = document.createElement("li");
+          span.className = "chegada-dia";
+          span.innerHTML = topic.segundaasexta.chegada[i];
+          chegadaSemana.appendChild(span);
+        }
+
+        for (let i = 0; i < topic.sabado.saida.length; i++) {
+          const span = document.createElement("li");
+          span.className = "saida-dia";
+          span.innerHTML = topic.sabado.saida[i];
+          saidaSabado.appendChild(span);
+        }
+        for (let i = 0; i < topic.sabado.chegada.length; i++) {
+          const span = document.createElement("li");
+          span.className = "chegada-dia";
+          span.innerHTML = topic.sabado.chegada[i];
+          chegadaSabado.appendChild(span);
+        }
+      }
+    });
+  } else {
+    seletor.classList.add("hide");
+    return;
+  }
+}
+
+optionHorarios();
+// pegaHorario(item);
+selectHorario.addEventListener("change", () => {
+  let item = parseInt(selectHorario.value);
+  pegaHorario(item);
+});
